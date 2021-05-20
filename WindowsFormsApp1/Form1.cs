a8a8edeb0a82d589b8ddefd0e02cc8a54b3d08a1
Max <max.main.mail@gmail.com>
2021-05-20 18:29:06 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var buff = new byte[4096];
+            var nodeRoot = new TreeNode("Buffers");
+            nodeRoot.Nodes.Add(new TreeNode(BitConverter.ToString(buff)));
+            nodeRoot.Nodes.Add(new TreeNode(BitConverter.ToString(buff)));
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(nodeRoot);
+        }
+    }
+}
---
+++ WindowsFormsAppBug/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppBug
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var buff = new byte[4096];
+            var nodeRoot = new TreeNode("Buffers");
+            nodeRoot.Nodes.Add(new TreeNode(BitConverter.ToString(buff)));
+            nodeRoot.Nodes.Add(new TreeNode(BitConverter.ToString(buff)));
+            nodeRoot.ExpandAll();
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(nodeRoot);
+        }
+    }
+}
